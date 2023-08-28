3663d880cab2a805de2225ed4996731783454e47
Robin Sen <robinsen2217@gmail.com>
2023-08-28 18:13:42 +0530

    added warp logo

--- src/Components/DadaFoot.jsx
+++ src/Components/DadaFoot.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function DadaFoot(props) {
     
   return (
-    <div className={`w-full bg-black text-center text-white mt-4 py-5 font-semibold ${props.style}`}>
+    <div className={`w-full bg-black text-center text-white mt-4 py-3 font-semibold ${props.style}`}>
       Designed by <span className='text-cyan-300'>WarP</span>
+      <img src="/mainLogo.jpg" alt="mainLogo" className='inline h-10'/>
     </div>
   )
 }
